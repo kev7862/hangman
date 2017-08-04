62eb9dfb51daf6aff08dae395e7a7c78a22b179e
kev7862 <kevmatite7862@gmail.com>
2017-08-04 15:30:01 +0300

    initialised the 'while' loop with the 'for' loop

--- HangMan.java
+++ HangMan.java
@@ -12,7 +12,11 @@ public class HangMan {
       System.out.println("Welcome to my Game of Hangman");
       char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].tocharArray();// This method Breaks down a word into a single Character.
       int amountOfGuesses = randomWordToGuess.length; //100
-      char[] playerGuess = new char[amountOfGuesses]; //This is where we save the users input.
+      char[] playerGuess = new char[amountOfGuesses]; //This is where we save the users input. e.g __ _r_ __ _p_
+
+      for (int i = 0; i < playerGuess.length; i++) {
+
+      }
 
 
     }
