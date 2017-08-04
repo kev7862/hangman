70fea91f902c2ae4a4470e17b622bfee41868b1e
kev7862 <kevmatite7862@gmail.com>
2017-08-04 15:02:03 +0300

    created a while loop and an Array where users can save input

--- HangMan.java
+++ HangMan.java
@@ -5,6 +5,18 @@ public class HangMan {
   public static void main ( String[] args ) {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
+    String[] guesses = {"reddit", "programming", "fruit", "donut"};
+
+    boolean weArePlaying = true;
+    while(weArePlaying) {
+      System.out.println("Welcome to my Game of Hangman");
+      char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].tocharArray();// This method Breaks down a word into a single Character.
+      int amountOfGuesses = randomWordToGuess.length; //100
+      char[] playerGuess = new char[amountOfGuesses]; //This is where we save the users input.
+
+
+    }
+    System.out.println("Game Over.");
 
   }
 }
