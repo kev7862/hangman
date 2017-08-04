55b082b40146ddeade2591df3b531429a44c9f9f
kev7862 <kevmatite7862@gmail.com>
2017-08-04 18:59:44 +0300

    Compiled and fixed emerging errors

--- HangMan.java
+++ HangMan.java
@@ -10,26 +10,25 @@ public class HangMan {
     boolean weArePlaying = true;
     while(weArePlaying) {
       System.out.println("Welcome to my Game of Hangman");
-      char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].tocharArray();// This method Breaks down a word into a single Character.
+      char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();// This method Breaks down a word into a single Character.
       int amountOfGuesses = randomWordToGuess.length; //100
       char[] playerGuess = new char[amountOfGuesses]; //This is where we save the users input. e.g __ _r_ __ _p_
 
       for (int i = 0; i < playerGuess.length; i++) {
-        playerGuess[i] = "__";
+        playerGuess[i] = '_';
       }
       //THIS IS THE LOOP WHERE THE GUESSING TAKES PLACE.
       boolean wordIsGuessed = false;
       int tries = 0;
 
-      while (!wordIsGuessed && tries != AmountOfGuesses) {
+      while (!wordIsGuessed && tries != amountOfGuesses) {
         System.out.print("Current guesses: ");
         printArray(playerGuess);
-        System.out.print("You have %d tries left.\n", amountOfGuesses - tries);
+        System.out.printf("You have %d tries left.\n", amountOfGuesses - tries);
         System.out.println("Enter a single character");//THIS IS WHERE YOU PROMPT THE USER FOR INPUT
         char input = scanner.nextLine().charAt(0);// THIS METHOD ONLY TAKES THE FIRST CHARACTER.
-
         tries ++;//ThIS IS WHERE YOU INCREAMENT USERS TRIES(if user inputs a "minus" we stop the game.)
-        if (input == "-") {
+        if (input == '_') {
           weArePlaying = false;
           wordIsGuessed = true;
         } else {
@@ -49,19 +48,19 @@ public class HangMan {
         String anotherGame = scanner.nextLine();
         if (anotherGame.equals("no"))  weArePlaying = false; // HERE WE ASK THE USER IF THEY WANT TO PLAY ANOTHER GAME.
     }
-    
+
     System.out.println("Game Over.");
 
   }
   public static void printArray(char[] array) {
-    for (int i = 0; i < playerGuess.length; i++) {
+    for (int i = 0; i < array.length; i++) {
       System.out.print(array[i] + " ");
     }
     System.out.println();
   }
   public static boolean isTheWordGuessed(char[] array) {  //THIS IS WHERE YOU IMPLEMENT THE GUESSED WORD.
-    for (int i = 0; i < playerGuess.length; i++) {
-      if (array[i] == "_") return false;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == '-') return false;
     }
     return true;
   }
