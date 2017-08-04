720333e055a844ade12074fdb90a94817b569a18
kev7862 <kevmatite7862@gmail.com>
2017-08-04 14:27:38 +0300

    Created a user input and a Random class. Also declared an Array.

---
+++ HangMan.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HangMan {
+  public static void main ( String[] args ) {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+  }
+}
